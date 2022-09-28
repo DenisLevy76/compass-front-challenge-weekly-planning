f0440ac3afcce459c926e48c721c382ffdf2dc6c
Denis Levy <denis.levi11@gmail.com>
2022-09-28 15:24:29 -0300

    Add createActivity function and localStorage configuration

---
+++ scripts/home.js
@@ -0,0 +1,23 @@
+const activity = document.querySelector('.form__activity');
+const day = document.querySelector('.form__select');
+const time = document.querySelector('.form__time');
+
+const data = JSON.parse(localStorage.getItem('data')) || { days: {} };
+console.log(data);
+const createActivity = (event) => {
+  event.preventDefault();
+
+  try {
+    data.days[day.value][time.value].push(activity.value);
+  } catch (error) {
+    data.days[day.value] = {
+      ...data.days[day.value],
+      [time.value]: [activity.value],
+    };
+  }
+
+  console.log(data);
+};
+
+const saveLocalStorage = () =>
+  localStorage.setItem('data', JSON.stringify(data));
