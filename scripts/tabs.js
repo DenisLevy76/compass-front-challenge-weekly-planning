5e9851c327da94fb6d7081da4434a711c030e749
Denis Levy <denis.levi11@gmail.com>
2022-09-29 14:29:52 -0300

    Add colors in tabs

--- scripts/tabs.js
+++ scripts/tabs.js
@@ -1,5 +1,3 @@
-import { data } from './home';
-
 const daysOfWeek = [
   'sunday',
   'monday',
@@ -10,24 +8,26 @@ const daysOfWeek = [
   'saturday',
 ];
 
-const translate = {
-  sunday: 'Domingo',
-  monday: 'Segunda-feira',
-  tuesday: 'Terça-feira',
-  wednesday: 'Quarta-feira',
-  thursday: 'Quinta-feira',
-  friday: 'Sexta-feira',
-  saturday: 'Sábado',
+const styles = {
+  sunday: { translate: 'Domingo', color: '#FFA246' },
+  monday: { translate: 'Segunda-feira', color: '#35E185' },
+  tuesday: { translate: 'Terça-feira', color: '#6688FF' },
+  wednesday: { translate: 'Quarta-feira', color: '#B266FF' },
+  thursday: { translate: 'Quinta-feira', color: '#66D1FF' },
+  friday: { translate: 'Sexta-feira', color: '#FF66D4' },
+  saturday: { translate: 'Sábado', color: '#FF6666' },
 };
 
 const navTabs = document.querySelector('.nav-tabs');
 
 daysOfWeek.forEach((day, index) => {
   const li = document.createElement('li');
-  li.innerHTML = `<li class="nav-item" role="presentation">
+  li.classList.add('nav-item');
+  li.innerHTML = `
   <button
     class="nav-link ${index === 0 ? 'active' : ''}"
     id="${day}-tab"
+    style="background-color: ${styles[day].color}; color: white;"
     data-bs-toggle="tab"
     data-bs-target="#${day}-tab-pane"
     type="button"
@@ -35,9 +35,8 @@ daysOfWeek.forEach((day, index) => {
     aria-controls="${day}-tab-pane"
     aria-selected="true"
   >
-    ${translate[day]}
+    ${styles[day].translate}
   </button>
-</li>
-<li class="nav-item" role="presentation">`;
+`;
   navTabs.appendChild(li);
 });
