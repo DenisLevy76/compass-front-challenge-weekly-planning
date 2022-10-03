9831e38c1ce894b29ad46e39dbbfbbaef01d60d0
Denis Levy <denis.levi11@gmail.com>
2022-10-03 13:37:43 -0300

    Add deleteCurrentDay function

--- scripts/home.js
+++ scripts/home.js
@@ -2,7 +2,14 @@ const activity = document.querySelector('.form__activity');
 const day = document.querySelector('.form__select');
 const time = document.querySelector('.form__time');
 
-const data = JSON.parse(localStorage.getItem('data')) || { days: {} };
+let data = JSON.parse(localStorage.getItem('data')) || { days: {} };
+
+const createTimeSelectOption = (value, label = '') => {
+  const option = document.createElement('option');
+  option.setAttribute('value', value);
+  option.innerText = label || value;
+  return option;
+};
 
 const createActivity = (event) => {
   event.preventDefault();
@@ -26,6 +33,11 @@ const deleteActivity = (day, hour, index) => {
   updateScreen();
 };
 
+const deleteCurrentDay = () => {
+  data.days[currentTab] = null;
+  updateScreen();
+};
+
 const saveLocalStorage = () => {
   localStorage.setItem('data', JSON.stringify(data));
   updateScreen();
@@ -33,5 +45,21 @@ const saveLocalStorage = () => {
 
 const resetLocalStorage = () => {
   localStorage.clear();
+  data = { days: {} };
   updateScreen();
 };
+
+var timeSkip = 1;
+var mins = ['00', '30'];
+
+for (let i = 0; i < 24; i += timeSkip) {
+  mins
+    .sort()
+    .forEach((min) =>
+      time.appendChild(
+        createTimeSelectOption(
+          `${i.toString().padStart(2, '0')}h${min.toString().padStart(2, '0')}m`
+        )
+      )
+    );
+}
